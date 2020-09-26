efd9cdc883431e0010c5faf7a20421f83e5d7d79
Sambhav Dave <sambhavd4@gmail.com>
2020-09-26 20:08:02 +0530

    First Commit.
    Signed-off-by: Sambhav Dave <sambhavd4@gmail.com>

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,12 @@
+import org.apache.log4j.Logger;
+
+/**
+ * Created By: Sambhav
+ * Created On: 26-09-2020 || 07:56 PM
+ * Project Name: test-automation-framework-utilities
+ **/
+public class BaseTest {
+    Logger log = Logger.getLogger(BaseTest.class);
+    //The above 'log' reference can be used in child test scripts for logging. Also, above snippet can be placed  wherever we want.
+    // ( preferably in a parent class.)
+}
